614cddb4916b4e6a1522b42b9ec845d4a037aebc
muhammad abubakar <muhammad.abubakar241996@gmail.com>
2026-07-13 01:59:00 +0500

    Fix server imports

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 const authRoutes = require("./src/routes/authRoutes");
 const transactionRoutes = require("./src/routes/transactionRoutes");
@@ -13,3 +15,20 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/budgets", budgetRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(
+        `Server running on port ${
+          process.env.PORT || 5000
+        }`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
